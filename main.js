669ccd883ca505fa1722fd9f5e2cc54bf2fa85d7
HorroGlitch <lverot@edenschool.fr>
2024-01-24 11:59:46 +0100

    making form

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
 import './style.css'
+let name = document.querySelector("#name");
+let email = document.querySelector("#email");
+
+let nameValue = name.input;
+let emailValue = email.value;
+
+console.log(nameValue)
+function validForm(name, email) {
+  console.log(name)
+  nameValue.addEventListener("change", ()=>{
+   
+    console.log("le champ name est valide");
+
+  })
+  email.addEventListener("change", ()=>{
+    if (mail) {
+      
+    }
+  })
+}
+validForm(nameValue, emailValue)
+// nameValue.addEventListener("change", ()=>{
+//   console.log(nameValue)
+// })
